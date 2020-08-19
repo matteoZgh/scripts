a506b516790576f16524065d816ad940bfef66b5
matteo <1434386636@qq.com>
2020-08-20 06:31:58 +0800

    update

--- vol-toggle.sh
+++ vol-toggle.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-/usr/bin/amixer set Master toggle
+STATUS=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
+if [ $STATUS == "off" ]; then
+	/usr/bin/amixer set Master toggle
+else
+	/usr/bin/amixer set Master toggle
+	/usr/bin/amixer set Front toggle
+	/usr/bin/amixer set Surround toggle
+	/usr/bin/amixer set Center toggle
+	/usr/bin/amixer set LFE toggle
+fi
 /home/matteo/scripts/dwm-bar/dwm_bar.sh
